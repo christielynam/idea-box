53b299db431a89f3b9997b618ed1b3af51ac5b47
Christie Lynam <christielynam@gmail.com>
2017-04-03 21:08:19 -0600

    add idea in scripts

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ $('.save-idea').on('click', function() {
   var $newIdea = new Idea($title, $body);
   prependIdea($newIdea);
   clearInputFields();
-});
+})
 
 function clearInputFields () {
   var $title = $('.title-storage');
@@ -12,7 +12,7 @@ function clearInputFields () {
   $title.val('');
   $body.val('');
   toggleSaveDisable();
-};
+}
 
 function Idea(id, title, body, quality) {
   this.id = id;
@@ -24,7 +24,17 @@ function Idea(id, title, body, quality) {
 function prependIdea(newIdea) {
   var $title = newIdea.title;
   var $body = newIdea.body;
+  var $quality = newIdea.quality;
   $('.idea-container').prepend(
-    `<article class="idea-card">`
+    `<article class="idea-card">
+      <h2>${$title}</h2>
+      <img class="delete-icon" src="assets/delete.svg" alt="delete-icon">
+      <p class="body-text">${$body}</p>
+      <div class="quality-container">
+        <img class="upvote-icon" src="assets/upvote.svg" alt="upvote"
+        <img class="downvote-icon" src="assets/downvote.svg" alt="downvote">
+        <p class="quality-text">${$quality}</p>
+      </div>
+    </article>`
   )
 }
