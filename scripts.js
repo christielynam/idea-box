04368f0952d7df65a7feaf16b32392d48740cd89
Joe Heitkamp <noetic97@gmail.com>
2017-04-03 23:56:29 -0600

    add missing div in prependIdea and quality vote function. Still WIP.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
-$('.save-idea').on('click', function() {
+$('.save-idea').on('click', function () {
   var $title = $('.title-storage').val();
   var $body = $('.body-storage').val();
   var $newIdea = new Idea($title, $body);
   prependIdea($newIdea);
   clearInputFields();
-})
+});
 
-function clearInputFields () {
+function clearInputFields() {
   var $title = $('.title-storage');
-  var $body = $('.body-storage')
+  var $body = $('.body-storage');
   $title.val('');
   $body.val('');
   toggleSaveDisable();
 }
+
+// function toggleSaveDisable() {
+//   $('.title-storage').on('keypress', function () {
+//     $('.save-idea').prop('disabled', false);
+//   });
+// }
 
 function Idea(id, title, body, quality) {
   this.id = id;
@@ -27,14 +33,24 @@ function prependIdea(newIdea) {
   var $quality = newIdea.quality;
   $('.idea-container').prepend(
     `<article class="idea-card">
-      <h2>${$title}</h2>
-      <img class="delete-icon" src="assets/delete.svg" alt="delete-icon">
+      <div class="card-header">
+        <h2>${$title}</h2>
+        <img class="delete-icon" src="assets/delete.svg" alt="delete-icon">
+      </div>
       <p class="body-text">${$body}</p>
       <div class="quality-container">
-        <img class="upvote-icon" src="assets/upvote.svg" alt="upvote"
+        <img class="upvote-icon" src="assets/upvote.svg" alt="upvote">
         <img class="downvote-icon" src="assets/downvote.svg" alt="downvote">
-        <p class="quality-text">${$quality}</p>
+        <p class="quality-text">quality: swill</p>
       </div>
     </article>`
-  )
+  );
+
+  $('.upvote-icon').on('click', function () {
+      $('.quality-text').text('quality: plausible');
+    });
+
+  $('.downvote-icon').on('click', function () {
+        $('.quality-text').text('quality: swill');
+      });
 }
